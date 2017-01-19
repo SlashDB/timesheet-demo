011b877f3b6c086d0bb30ffb10811f7564f6f0be
Krzysztof Malinowski <boromil@gmail.com>
2017-01-19 11:42:10 +0100

    timesheet form

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,65 @@
 (function () {
     'use strict';
 
-    function getURL(s) {
+    Date.prototype.toDateTimeInputValue = (function () {
+        var local = new Date(this);
+        local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+        return local.toJSON().slice(0, 16);
+    });
+
+    var getURL = function (s) {
         return window.timesheet.baseURL + s;
-    }
+    };
+
+    // some utility functions
+
+    var msToH = function (ms) {
+        // converts milliseconds to hours
+        return ms * 2.777777777777778e-7;
+    };
+
+    var resetFields = function (target, items) {
+        var item;
+        for (var i = 0, l = items.length; i < l; i++) {
+            item = target[items[i]];
+            item.errors = [];
+            item.value = '';
+        }
+    };
+
+    var checkIfValid = function (predicate, errors, errMsg) {
+        var errMsgIdx = errors.indexOf(errMsg),
+            hasErrorMsg = errMsgIdx > -1;
+
+        if (predicate) {
+            if (!hasErrorMsg) {
+                errors.push(errMsg);
+            }
+            return false;
+        } else if (hasErrorMsg) {
+            errors.splice(errMsgIdx, 1);
+        }
+        return true;
+    };
+
+    var formValid = function (obj) {
+        var formValid = true,
+            keys = Object.keys(obj),
+            reqMsg = 'this field is required',
+            field;
+
+        var predicateFn = function (f) {
+            return (f.required || false) && !Boolean(f.value);
+        };
+
+        for (var i = 0, l = keys.length; i < l; i++) {
+            field = obj[keys[i]];
+            if (!checkIfValid(predicateFn(field), field.errors, reqMsg)) {
+                formValid = false;
+            }
+        }
+        return formValid;
+    };
 
     Vue.component('InputErrors', {
         template: '<div><div v-for="e in errors" class="form-control-feedback">{{ e }}</div></div>',
@@ -16,37 +72,6 @@
             }
         }
     });
-
-    var resetFields = function (target, items) {
-        var item;
-        for (var i = 0, l = items.length; i < l; i++) {
-            item = target[items[i]];
-            item.errors = [];
-            item.value = '';
-        }
-    };
-
-    var formValid = function (obj) {
-        var formValid = true,
-            keys = Object.keys(obj),
-            reqMsg = 'this field is required',
-            field, reqMsgIdx;
-
-        for (var i = 0, l = keys.length; i < l; i++) {
-            field = obj[keys[i]];
-            reqMsgIdx = field.errors.indexOf(reqMsg);
-
-            if ((field.required || false) && !Boolean(field.value)) {
-                if (reqMsgIdx === -1) {
-                    field.errors.push(reqMsg);
-                }
-                formValid = false;
-            } else {
-                field.errors.splice(reqMsgIdx, 1);
-            }
-        }
-        return formValid;
-    };
 
     Vue.component('LoginForm', {
         template: `
@@ -286,6 +311,141 @@
         }
     });
 
+    Vue.component('NewTimesheet', {
+        template: `
+        <div class="card text-center">
+            <div class="card-block">
+                <form @submit.prevent="create">
+                    <div class="form-group" :class="{'has-danger': durationFrom.errors.length > 0}">
+                        <label for="duration-from">Duration from</label>
+                        <input type="datetime-local"
+                               v-model="durationFrom.value"
+                               class="form-control form-control-sm" id="duration-from"
+                               :class="{'form-control-danger': durationFrom.errors.length > 0}"
+                               aria-describedby="durationFromHelp">
+                        <input-errors :errors="durationFrom.errors"/>
+                    </div>
+                    <div class="form-group" :class="{'has-danger': durationTo.errors.length > 0}">
+                        <label for="duration-from">to</label>
+                        <input type="datetime-local"
+                               v-model="durationTo.value"
+                               class="form-control form-control-sm" id="duration-from"
+                               :class="{'form-control-danger': durationTo.errors.length > 0}"
+                               aria-describedby="durationToHelp">
+                        <input-errors :errors="durationTo.errors"/>
+                    </div>
+                    <div class="form-group" :class="{'has-danger': accomplishments.errors.length > 0}">
+                        <label for="accomplishments">Accomplishments</label>
+                        <textarea
+                            class="form-control form-control-sm"
+                            :class="{'form-control-danger': accomplishments.errors.length > 0}"
+                            v-model="accomplishments.value"
+                            id="accomplishments" rows="3">
+                        </textarea>
+                        <input-errors :errors="accomplishments.errors"/>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Create</button>
+                </form>
+            </div>
+        </div>
+        `,
+        methods: {
+            getDateTimeNow: function (minOffset) {
+                if (minOffset == null) {
+                    minOffset = 0;
+                }
+                var now = new Date();
+                now.setMinutes(now.getMinutes() + minOffset);
+                return now.toDateTimeInputValue();
+            },
+            localFormValid: function () {
+                var tmp = formValid(this._data);
+                if (!tmp) {
+                    return false;
+                }
+
+                var from = Date.parse(this.durationFrom.value),
+                    to = Date.parse(this.durationTo.value),
+                    dateTimeInvalidMsg = 'enter a valid date and time';
+
+                if (!checkIfValid(isNaN(from), this.durationFrom.errors, dateTimeInvalidMsg)) {
+                    return false;
+                }
+
+                if (!checkIfValid(isNaN(to), this.durationTo.errors, dateTimeInvalidMsg)) {
+                    return false;
+                }
+
+                if (!checkIfValid(from > to, this.durationFrom.errors, 'from must be before to')) {
+                    return false;
+                }
+
+                // lets set duration
+                this.duration.value = Number(msToH(to - from).toFixed(3));
+                return true;
+            },
+            create: function () {
+                var t = this;
+
+                if (t.localFormValid()) {
+                    var data = {
+                        duration: t.duration.value,
+                        accomplishments: t.accomplishments.value,
+                        project_id: t.projectId,
+                        user_id: t.userId
+                    };
+
+                    $.ajax({
+                        url: getURL('/timesheet.json'),
+                        type: "POST",
+                        data: JSON.stringify(data),
+                        contentType: "application/json; charset=utf-8",
+                        error: function (resp) {
+                            // ignore errors - bitbucket issue #360
+                            var ld = {
+                                data: $.extend({}, data),
+                                timesheets: []
+                            };
+                            t.$emit('timesheet-created', ld);
+                            resetFields(t, ['durationFrom', 'durationTo', 'accomplishments']);
+                        }
+                    });
+                }
+            }
+        },
+        data: function () {
+            return {
+                durationFrom: {
+                    value: this.getDateTimeNow(),
+                    errors: []
+                },
+                durationTo: {
+                    value: this.getDateTimeNow(15),
+                    errors: []
+                },
+                duration: {
+                    value: 0.1,
+                    errors: []
+                },
+                accomplishments: {
+                    value: '',
+                    errors: [],
+                    required: true
+                }
+            };
+        },
+        props: {
+            userId: {
+                type: Number,
+                default: -1,
+            },
+            projectId: {
+                type: Number,
+                default: -1,
+            }
+        }
+    });
+
     Vue.component('NewProject', {
         template: `
         <div class="card text-center">
@@ -295,10 +455,10 @@
                     <div class="form-group" :class="{'has-danger': name.errors.length > 0}">
                         <label for="name">Name</label>
                         <input type="name"
-                               class="form-control form-control-sm"id="name"
+                               class="form-control form-control-sm" id="name"
                                :class="{'form-control-danger': name.errors.length > 0}"
                                v-model="name.value"
-                               aria-describedby="emailHelp" placeholder="enter a project name">
+                               aria-describedby="nameHelp" placeholder="enter a project name">
                         <input-errors :errors="name.errors"/>
                     </div>
                     <div class="form-group" :class="{'has-danger': description.errors.length > 0}">
@@ -392,8 +552,8 @@
         template: `
         <div class="mt-3">
             <new-project :userId="userId" @project-created="addProject"/>
-            <div v-if="pids.length > 0" class="mt-3">
-                <div class="card mb-3" v-for="pid in pids">
+            <div v-if="pids.length > 0">
+                <div class="card mt-3" v-for="pid in pids">
                     <div class="card-header">
                         <span><strong>{{ projects[pid].data.name }}</strong></span>
                         <span class="float-sm-right float-md-right float-lg-right">
@@ -401,7 +561,8 @@
                         </span>
                     </div>
                     <div class="card-block">
-                        <div class="card" v-for="timesheet in projects[pid].timesheets">
+                        <new-timesheet :userId="userId" :projectId="Number(pid)"/>
+                        <div class="card mt-2" v-for="timesheet in projects[pid].timesheets">
                             <div class="card-block">
                                 <form>
                                     <fieldset disabled readonly>
